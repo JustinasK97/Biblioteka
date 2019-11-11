1fc70d1f6824ff6e3f635fd7cf07ce1669fd5e0c
Arnoldas <support@coincrate.net>
2019-11-11 18:59:44 +0200

    biblioteka

--- js/main.js
+++ js/main.js
@@ -1,81 +1,96 @@
-"use strict"
-let knygos[
-	grozine[{
 
-		ISBN: 52648
-		leidimoMetai: 2000
-		pavadinimas: "Pasakų šaly"
-		puslapiuSkaicius: 236
-	},
+let knygos =
+[
 	{
-		ISBN: 59231
-		leidimoMetai: 2015
-		pavadinimas: "Žaidimų aikštelė"
-		puslapiuSkaicius: 365
-	},
-	{
-		ISBN: 69853
-		leidimoMetai: 1997
-		pavadinimas: "Nežinomasis"
-		puslapiuSkaicius: 302
-	},
-	{
-		ISBN: 89564
-		leidimoMetai: 2005
-		pavadinimas: "Oras"
-		puslapiuSkaicius: 400
-	}
-	],
-	Romanas[{
+			'Grožinė':[{
 
-		ISBN: 54812
-		leidimoMetai: 2019("Nauja knyga")
-		pavadinimas: "Dviračių karalius"
-		puslapiuSkaicius: 569
+				isbn: "52648",
+				leidimoMetai: "2000",
+				pavadinimas: "Pasakų šaly",
+				puslapiuSkaicius: "236"
+		},
+		{
+				isbn: "59231",
+				leidimoMetai: "2015",
+				pavadinimas: "Žaidimų aikštelė",
+				puslapiuSkaicius: "365"
+		},
+		{
+				isbn: "69853",
+				leidimoMetai: "1997",
+				pavadinimas: "Nežinomasis",
+				puslapiuSkaicius: "302"
+		},
+		{
+				isbn: "89564",
+				leidimoMetai: "2005",
+				pavadinimas: "Oras",
+				puslapiuSkaicius: "400"
+		}
+		]
 	},
 	{
-		ISBN: 69853
-		leidimoMetai: 2016
-		pavadinimas: "Tarp dviejų pasaulių"
-		puslapiuSkaicius: 453
-	},
-	{
-		ISBN: 69852
-		leidimoMetai: 2009
-		pavadinimas: "Melo pradžiamokslis"
-		puslapiuSkaicius: 598
-	},
-	{
-		ISBN: 69420
-		leidimoMetai: 2008
-		pavadinimas: "Laukiniai arkliai"
-		puslapiuSkaicius: 689
-	}
-	],
-	Drama[{
+			'Romanas':[{
 
-		ISBN: 78512
-		leidimoMetai: 1999
-		pavadinimas: "Nuobodulys"
-		puslapiuSkaicius: 780
+				isbn: "54812",
+				leidimoMetai: "2019",
+				pavadinimas: "Dviračių karalius",
+				puslapiuSkaicius: "569"
+		},
+		{
+				isbn: "69853",
+				leidimoMetai: "2016",
+				pavadinimas: "Tarp dviejų pasaulių",
+				puslapiuSkaicius: "453"
+		},
+		{
+				isbn: "69852",
+				leidimoMetai: "2009",
+				pavadinimas: "Melo pradžiamokslis",
+				puslapiuSkaicius: "598"
+		},
+		{
+				isbn: "69420",
+				leidimoMetai: "2008",
+				pavadinimas: "Laukiniai arkliai",
+				puslapiuSkaicius: "689"
+		}
+		]
 	},
 	{
-		ISBN: 84562
-		leidimoMetai: 2001
-		pavadinimas: "Toliau nuo čia"
-		puslapiuSkaicius: 326
-	},
-	{
-		ISBN: 25461
-		leidimoMetai: 2002
-		pavadinimas: "Pabaigos pradžia"
-		puslapiuSkaicius: 245
-	},
-	{
-		ISBN: 35629
-		leidimoMetai: 2017
-		pavadinimas: "Obuolys"
-		puslapiuSkaicius: 560
+			'Drama':[{
+
+				isbn: "78512",
+				leidimoMetai: "1999",
+				pavadinimas: "Nuobodulys",
+				puslapiuSkaicius: "780"
+		},
+		{
+				isbn: "84562",
+				leidimoMetai: "2001",
+				pavadinimas: "Toliau nuo čia",
+				puslapiuSkaicius: "326"
+		},
+		{
+				isbn: "25461",
+				leidimoMetai: "2002",
+				pavadinimas: "Pabaigos pradžia",
+				puslapiuSkaicius: "245"
+		},
+		{
+				isbn: "35629",
+				leidimoMetai: "2017",
+				pavadinimas: "Obuolys",
+				puslapiuSkaicius: "560"
+		}
+		]
 	}
-	]
 ]
+for(kategorija of knygos){
+	for(duomenys in kategorija)
+			console.log("Kategorija - " + duomenys)
+
+	for(apie in duomenys)
+		for(info in kategorija[duomenys][apie])
+			console.log(info + ' : ' + kategorija[duomenys][apie][info])
+}
