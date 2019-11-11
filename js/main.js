a87d21331134894fe81ecf4cf124c9eaf80f50c4
JustinasK97 <Justinask97@gmail.com>
2019-11-11 10:32:46 +0200

    biblioteka

--- js/main.js
+++ js/main.js
@@ -3,11 +3,79 @@ let knygos[
 	grozine[{
 
 		ISBN: 52648
-		Leidimo Metai: 2000
-		Pavadinimas: "Pasakų šaly"
-		Puslapiu skaicius: 236
+		leidimoMetai: 2000
+		pavadinimas: "Pasakų šaly"
+		puslapiuSkaicius: 236
 	},
+	{
+		ISBN: 59231
+		leidimoMetai: 2015
+		pavadinimas: "Žaidimų aikštelė"
+		puslapiuSkaicius: 365
+	},
+	{
+		ISBN: 69853
+		leidimoMetai: 1997
+		pavadinimas: "Nežinomasis"
+		puslapiuSkaicius: 302
+	},
+	{
+		ISBN: 89564
+		leidimoMetai: 2005
+		pavadinimas: "Oras"
+		puslapiuSkaicius: 400
+	}
+	],
+	Romanas[{
 
+		ISBN: 54812
+		leidimoMetai: 2019("Nauja knyga")
+		pavadinimas: "Dviračių karalius"
+		puslapiuSkaicius: 569
+	},
+	{
+		ISBN: 69853
+		leidimoMetai: 2016
+		pavadinimas: "Tarp dviejų pasaulių"
+		puslapiuSkaicius: 453
+	},
+	{
+		ISBN: 69852
+		leidimoMetai: 2009
+		pavadinimas: "Melo pradžiamokslis"
+		puslapiuSkaicius: 598
+	},
+	{
+		ISBN: 69420
+		leidimoMetai: 2008
+		pavadinimas: "Laukiniai arkliai"
+		puslapiuSkaicius: 689
+	}
+	],
+	Drama[{
 
+		ISBN: 78512
+		leidimoMetai: 1999
+		pavadinimas: "Nuobodulys"
+		puslapiuSkaicius: 780
+	},
+	{
+		ISBN: 84562
+		leidimoMetai: 2001
+		pavadinimas: "Toliau nuo čia"
+		puslapiuSkaicius: 326
+	},
+	{
+		ISBN: 25461
+		leidimoMetai: 2002
+		pavadinimas: "Pabaigos pradžia"
+		puslapiuSkaicius: 245
+	},
+	{
+		ISBN: 35629
+		leidimoMetai: 2017
+		pavadinimas: "Obuolys"
+		puslapiuSkaicius: 560
+	}
 	]
 ]
